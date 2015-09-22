d7a3607b2a7e4678cbe7eb6df57e0d3bfe0aec5d
Kerry Buckley <kerryjbuckley@gmail.com>
2015-09-22 21:14:14 +0100

    Seems to work

--- lib/multi_iterator.rb
+++ lib/multi_iterator.rb
@@ -6,9 +6,22 @@ class MultiIterator
   end
 
   def each
-    8.times do
-      next_index = @iterators.map(&:peek).each_with_index.sort.first.last
-      yield @iterators[next_index].next
+    loop do
+      values = next_values
+      break if values.empty?
+      yield values.sort_by {|k,v| v }.first.first.next
+    end
+  end
+
+  private
+
+  def next_values
+    @iterators.each_with_object({}) do |iterator, values|
+      begin
+        values[iterator] = iterator.peek
+      rescue StopIteration
+        next
+      end
     end
   end
 end
--- spec/multi_iterator_spec.rb
+++ spec/multi_iterator_spec.rb
@@ -2,7 +2,7 @@ require "multi_iterator"
 
 describe MultiIterator do
   it "interleaves multiple pre-sorted iterators in order" do
-    iterator = described_class.new (1..5).each, [1, 1, 2, 3, 5, 8, 13].each, [4].each
+    iterator = described_class.new [].each, (1..5).each, [1, 1, 2, 3, 5, 8, 13].each, [4].each
     expect(iterator.to_enum.to_a).to eq [1, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 8, 13]
   end
 end
