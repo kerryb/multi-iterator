0a47e55270a93448695447adab43c53a278afced
Kerry Buckley <kerryjbuckley@gmail.com>
2015-09-23 22:53:24 +0100

    Add test for infinite iterators

--- spec/multi_iterator_spec.rb
+++ spec/multi_iterator_spec.rb
@@ -6,6 +6,11 @@ describe MultiIterator do
     expect(iterator.to_enum.to_a).to eq [1, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 8, 13]
   end
 
+  it "works with infinite iterators" do
+    iterator = described_class.new (1..Float::INFINITY).each, [1.5, 2.5].each
+    expect(iterator.take 6).to eq [1, 1.5, 2, 2.5, 3, 4]
+  end
+
   it "raises IteratorOutOfOrder if an iterator isn't already sorted" do
     iterator = described_class.new [2, 1].each
     expect { iterator.to_enum.to_a }.to raise_error { MultiIterator::IteratorOutOfOrder }
